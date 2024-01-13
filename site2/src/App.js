0d217cceae15538f466d294771a84932ac0360c0
Wheat <jjustinxiao@gmail.com>
2024-01-13 15:08:33 -0800

    adding untracked

--- site2/src/App.js
+++ site2/src/App.js
@@ -1,7 +1,7 @@
 // App.js
 
 import React from 'react';
-import Circle from './Circle'
+import Circle from './Circle';
 
 function App() {
   return (
--- site2/src/Circle.js
+++ site2/src/Circle.js
@@ -3,7 +3,42 @@
 import React from 'react';
 
 class Circle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showExperience: false,
+      showSettings: false,
+    };
+
+    // Bind the event handlers to the instance
+    this.handleExperienceClick = this.handleExperienceClick.bind(this);
+    this.handleSettingsClick = this.handleSettingsClick.bind(this);
+    this.handleEmergencyClick = this.handleEmergencyClick.bind(this);
+    this.handleFriendClick = this.handleFriendClick.bind(this);
+  }
+
+  handleExperienceClick() {
+    alert('Experience clicked');
+    this.setState({ showExperience: true, showSettings: false });
+  }
+
+  handleSettingsClick() {
+    alert('Settings clicked');
+    this.setState({ showSettings: true, showExperience: false });
+  }
+
+  handleEmergencyClick() {
+    alert('Emergency services called');
+  }
+
+  handleFriendClick() {
+    alert('A friend has been contacted');
+  }
+
   render() {
+    const { showExperience, showSettings } = this.state;
+
     const containerStyle = {
       position: 'relative',
       height: '100vh', // Set the container height to the full viewport height
@@ -11,18 +46,27 @@ class Circle extends React.Component {
 
     const circleContainerStyle = {
       position: 'absolute',
-      top: '50%',
+      top: '40%',
       left: '50%',
       transform: 'translate(-50%, -50%)',
       textAlign: 'center',
     };
 
-    const circleStyle = {
-      width: '60vmin',
-      height: '60vmin',
+    const BACcircleStyle = {
+      width: '52vmin',
+      height: '52vmin',
       borderRadius: '50%',
       backgroundColor: 'blue', // You can change the color as desired
       margin: '0 auto', // Center the circle horizontally
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+    };
+
+    const BACtextStyle = {
+      color: 'white',
+
     };
 
     const topLeftCircleStyle = {
@@ -33,6 +77,7 @@ class Circle extends React.Component {
       position: 'absolute',
       top: '0',
       left: '0',
+      cursor: 'pointer',
     };
 
     const topRightCircleStyle = {
@@ -43,6 +88,41 @@ class Circle extends React.Component {
       position: 'absolute',
       top: '0',
       right: '0',
+      cursor: 'pointer',
+    };
+
+    const expCircleStyle = {
+      width: '27vmin',
+      height: '27vmin',
+      borderRadius: '50%',
+      backgroundColor: showExperience ? 'darkblue' : 'green', // Change color when clicked
+      position: 'absolute',
+      bottom: '0',
+      left: '0',
+      cursor: 'pointer',
+    };
+
+    const settingsCircleStyle = {
+      width: '27vmin',
+      height: '27vmin',
+      borderRadius: '50%',
+      backgroundColor: showSettings ? 'darkred' : 'red', // Change color when clicked
+      position: 'absolute',
+      bottom: '0',
+      right: '0',
+      cursor: 'pointer',
+    };
+
+    const shotglassStyle = {
+      width: '27vmin',
+      height: '27vmin',
+      borderRadius: '50%',
+      backgroundColor: 'pink', // You can change the color as desired
+      position: 'absolute',
+      bottom: '0',
+      left: '50%',
+      transform: 'translateX(-50%)', // Center the pink circle horizontally
+      cursor: 'pointer',
     };
 
     const textStyle = {
@@ -50,26 +130,31 @@ class Circle extends React.Component {
     };
 
     const ringStyle = {
-        width: '133vw',
-        height: '133vw',
-        borderRadius: '50%',
-        border: '10px solid orange', // You can change the color and size as desired
-        position: 'absolute',
-        bottom: '0',
-        left: '50%',
-        transform: 'translateX(-50%)',
-      };
+      width: '185vw',
+      height: '185vw',
+      borderRadius: '50%',
+      border: '10px solid orange', // You can change the color and size as desired
+      position: 'absolute',
+      bottom: '-150vw',
+      left: '50%',
+      transform: 'translateX(-50%)',
+    };
 
     return (
-      <div style={containerStyle}>
+      <div classname='Circle'style={containerStyle}>
+      <div style={ringStyle}></div>
         <div style={circleContainerStyle}>
-        <div style={textStyle}>Your Text Here</div>
-          <div style={circleStyle}></div>
-        <div style={textStyle}>Your Text Here</div>
+          <div class='GUI' style={textStyle}>Your BAC is</div>
+          <div style={BACcircleStyle}>
+          <div id='BAC' style={BACtextStyle}>Centered Text</div>
+          </div>
+          <div class='GUI' style={textStyle}>You are BOOZIN</div>
         </div>
-        <div style={topLeftCircleStyle}></div>
-        <div style={topRightCircleStyle}></div>
-        <div style = {ringStyle}></div>
+        <div style={topLeftCircleStyle} onClick={this.handleEmergencyClick}></div>
+        <div style={topRightCircleStyle} onClick={this.handleFriendClick}></div>
+        <div style={expCircleStyle} onClick={this.handleExperienceClick}></div>
+        <div style={settingsCircleStyle} onClick={this.handleSettingsClick}></div>
+        <div style={shotglassStyle}></div>
       </div>
     );
   }
