bd8eef23f3b077b0b98b3337ac44f5ff4ea7e863
jacobfingerman <jacobfingerman@ucs.edu>
2024-01-13 22:47:56 -0800

    cut out useless garbage

--- site2/src/index.js
+++ site2/src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 document.body.classList.add("no-scroll");
 root.render(
-    <GoogleOAuthProvider clientId="37139050386-451rqkoosero3epoklqfsf712ovpkls2.apps.googleusercontent.com">
         <React.StrictMode>
             <App />
         </React.StrictMode>,
-    </GoogleOAuthProvider>,
     document.getElementById('root')
 );
 
